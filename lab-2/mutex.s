1c1cb34a9cd6d19561ac0cc04a87c13d775a5a40
yenchen <th33ts@gmail.com>
2014-08-05 10:55:38 +0800

    lab2 done

--- lab-2/mutex.s
+++ lab-2/mutex.s
@@ -9,7 +9,12 @@
 	.type lock_mutex, function
 lock_mutex:
         @ INSERT CODE BELOW
-
+		mov r3, #locked
+		ldrex r1, [r0]         @ get the state of mutex
+		teq r1, #unlocked      @ is mutex unlock?
+		strexeq r2, r3, [r0]   @try to locked the mutex
+		teqeq r2, #0           @ test locking mutex is success or not
+		bne lock_mutex         @ if mutex is not unlocked or it fails to lock the mutex, try again
         @ END CODE INSERT
 	bx lr
 
@@ -18,8 +23,9 @@ lock_mutex:
 	.global unlock_mutex
 	.type unlock_mutex, function
 unlock_mutex:
-	@ INSERT CODE BELOW
-        
+		@ INSERT CODE BELOW
+		mov r1, #unlocked
+		str r1, [r0]
         @ END CODE INSERT
 	bx lr
 	.size unlock_mutex, .-unlock_mutex
